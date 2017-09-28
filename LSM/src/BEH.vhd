6bde48d098e6ad6d04c4a611e5310fbd0e574483
Valeriy <KirinTor830@gmail.com>
2017-09-28 20:02:55 +0300

    Signed operations implemented

--- LSM/src/BEH.vhd
+++ LSM/src/BEH.vhd
@@ -1,20 +1,45 @@
-use CNetwork.all, FUN.all;
+--use CNetwork.all, FUN.all;	
+library ieee;
+use ieee.numeric_bit.all;	
+
+entity LSM is
+port(F : in BIT;-- function
+	 A : in BIT_VECTOR(11 downto 0);-- first operand
+	 B : in BIT_VECTOR(11 downto 0);-- second operand
+	 C0: in BIT; -- transfer port
+	 Y : out BIT_VECTOR(11 downto 0);-- result
+	 C3: out BIT; -- transfer output
+	 Z: out BIT); -- a sign of zero result
+end LSM;
+
 architecture BEH of LSM is
-signal ai, bi, ci, yi: INTEGER;
-signal ybi: BIT_VECTOR (12 downto 0);
+signal  yi: signed(12 downto 0);
+signal ai, bi, ci: signed(11 downto 0);
+signal ybi: BIT_VECTOR (12 downto 0);	
+
+function MIN(a,b:signed)return signed is
+begin			  
+	if a> b then
+		return b;
+	else
+		return a;
+		end if;
+   
+	end function ;
+
 begin
 -- Present the input data as integers --
-ai <= BIT_TO_INT (A);
-bi <= BIT_TO_INT (B);
-ci <= BIT_TO_INT (C0);
+ai <= signed(A);
+bi <= signed(B);
+--ci <= BIT_TO_INT (C0);
 -- Adder - subtractor --
-ADDER: yi <= ai + bi + ci when F = '0';
+ADDER: yi <= RESIZE(ai,13) + bi + signed('0'&ci) when F = '0';
 -- Results multiplexer --
 MUX: with F select
 
-ybi <= INT_TO_BIT (yi, 13) when '0', -- adder
+ybi <= bit_vector(yi) when '0', -- adder
 
- INT_TO_BIT (minimum(ai,bi), 13) when others;
+ bit_vector(MIN(ai,bi)) when others;
  C3 <= ybi (12); -- output transfer
  Z <= '1' when ybi (11 downto 0) = "000000000000" else '0'; -- zero sign
  Y <= ybi (11 downto 0); -- Result
